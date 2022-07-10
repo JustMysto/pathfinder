5ff33d99c400aac5c0d476d00744d6756506a28a
JustMysto <xxtouchtv@gmail.com>
2022-07-10 03:55:04 +0200

    add basic ui

---
+++ src/components/gridContainer.vue
@@ -0,0 +1,27 @@
+<script setup>
+	import { ref } from "vue"
+	import GridCell from "./gridCell.vue"
+
+	defineProps({
+		amount: Number,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+	<div class="grid-container">
+		<template v-for="index in 300" :key="index">
+			<GridCell/>
+		</template>
+	</div>
+</template>
+
+<style scoped>
+	.grid-container {
+		display: grid;
+		grid-template-columns: repeat(30, 25px);
+		gap: 5px;
+		justify-content: center;
+	}
+</style>
